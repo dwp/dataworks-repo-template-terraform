4a72ec1c93b10a42b37dc2c2ade03541bab9b596
Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
2020-05-27 15:13:41 +0100

    Added aviator to script
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>

--- rename.sh
+++ rename.sh
@@ -8,11 +8,14 @@ if [ ! -z 'grep -rnw $TF_REPO_NAME ci' ]; then
     git branch initial-commit
     find ci -type f -exec sed -i 's/$TF_REPO_NAME/$NEW_REPO_NAME/g' {} +
     find terraform -type f -exec sed -i 's/$TF_REPO_NAME/$NEW_REPO_NAME/g' {} +
+    find aviator.yml -type f -exec sed -i 's/$TF_REPO_NAME/$NEW_REPO_NAME/g' {} +
     rm rename.sh
     
-    git add --all
+    git add --quiet --all
     git diff --quiet && git diff --staged --quiet || git commit -m "Renamed pipeline to fit repository"
-    git push --set-upstream origin initial-commit
+    git push --quiet --set-upstream origin initial-commit
+    
+    aviator -s
 else
     echo "Not required, or not a Terraform repository"
     exit 1
