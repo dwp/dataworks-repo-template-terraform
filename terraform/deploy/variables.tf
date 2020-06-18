5abaf3a50ab302af72c38b419b215ed9cce9933f
Liam Rae-McLauchlan <liam.rae-mclauchlan@infinityworks.com>
2020-06-18 14:32:54 +0100

    Add costcode var

---
+++ terraform/deploy/variables.tf
@@ -0,0 +1,14 @@
+variable "assume_role" {
+  type    = string
+  default = "ci"
+}
+
+variable "region" {
+  type    = string
+  default = "eu-west-2"
+}
+
+variable "costcode" {
+  type    = string
+  default = ""
+}
